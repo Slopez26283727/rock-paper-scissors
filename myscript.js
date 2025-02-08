fa1960a124c260bf5b2c6cb00b0186c30cce9366
Slopez26283727 <150194360+Slopez26283727@users.noreply.github.com.>
2025-02-08 17:59:59 -0600

    addded code for computer and user choice

--- myscript.js
+++ myscript.js
@@ -1,1 +1,28 @@
-console.log("Hello World")
+function getComputerChoice(){
+    let int= Math.random()*2
+     if (int = 0){
+      return "Rock"
+     }
+     else if (int=1){
+     return "Scissors"
+     }
+     else {
+      return "Paper"
+     }
+  };
+  function getHumanChoice(){
+  let choice = prompt("Choose Rock Paper or Scissors")
+    if (choice.toLowerCase() === "rock"){
+      return "Rock"
+    }
+    else if (choice.toLowerCase() === "scissors"){
+      return "Scissors"
+    }
+    else {
+      return "Paper"
+    }
+  };
+  let Human=getHumanChoice()
+  console.log(Human)
+  let Computer=getComputerChoice()
+  console.log(Computer) 
